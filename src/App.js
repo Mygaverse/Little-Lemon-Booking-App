3f0844063d119c3c5f06e461105d836e06329d00
Mygaverse <myagaverse@gmail.com>
2024-05-09 11:51:53 +0800

    uploading all changed files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import Nav from "./components/Nav";
+import Hero from "./components/Hero";
+import Highlight from "./components/Highlight";
+import Testimonial from "./components/Testimonial";
+import About from "./components/About";
+import Footer from "./components/Footer";
 
 function App() {
   return (
-    <div className="App">
-      Homepage
-    </div>
+    <main>
+      <Nav />
+      <Hero />
+      <Highlight />
+      <Testimonial />
+      <About />
+      <Footer />
+    </main>
   );
 }
 
---
+++ src/components/Nav.js
@@ -0,0 +1,16 @@
+import logoImage from "../images/Logo.png";
+
+const Nav = () => {
+    return (
+        <header>
+            <nav>
+                <img src={logoImage} alt="Little Lemon logo"></img>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        </header>
+    );
+};
+
+export default Nav;
